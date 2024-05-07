187a32d0c308d924e14e137dabfad8ffa2ea1067
Mike Jonas <motion@gmail.com>
2024-05-07 05:07:00 -0700

    Organize sentry better

---
+++ Res/Managers/SentryManager.swift
@@ -0,0 +1,51 @@
+import Foundation
+import Sentry
+
+class SentryManager {
+    static let shared = SentryManager()
+    private init() {}
+    private var enableDebugLogging = false
+
+    // enableDebugLogging sends logs to sentry during local development (Debug builds)
+    func start(enableDebugLogging: Bool = false) {
+        self.enableDebugLogging = enableDebugLogging
+
+        // Check if Sentry should be active considering the debug flag
+        if Config.buildConfiguration == "Release" || enableDebugLogging {
+            SentrySDK.start { options in
+                options.dsn = Config.SENTRY_DSN
+                options.debug = enableDebugLogging  // Control SDK debugging with the parameter
+                options.enableTracing = true
+            }
+        }
+    }
+
+    
+    var isSentryActive: Bool {
+        return Config.buildConfiguration == "Release" || enableDebugLogging
+    }
+
+    func captureError(_ error: Error, description: String? = nil) {
+        if isSentryActive {
+            SentrySDK.capture(error: error) { scope in
+                if let description = description {
+                    scope.setExtra(value: description, key: "Description")
+                }
+            }
+        } else {
+            if let description = description {
+                print("Error: \(error.localizedDescription), Description: \(description)")
+            } else {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    func captureMessage(_ message: String) {
+        if isSentryActive {
+            SentrySDK.capture(message: message)
+        } else {
+            print("Message: \(message)")
+        }
+    }
+}
--- Res/ResApp.swift
+++ Res/ResApp.swift
@@ -12,24 +12,11 @@ import Sentry
 @main
 struct ResApp: App {
     init() {
-        SentrySDK.start { options in
-            options.dsn = Config.SENTRY_DSN
-            options.debug = true // Enabled debug when first installing is always helpful
-            options.enableTracing = true 
-
-            // Uncomment the following lines to add more data to the events
-            // options.attachScreenshot = true // This adds a screenshot to the error events
-            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
-        }
-
-        // Distinguish between build types (debug and release for now)
-        SentrySDK.configureScope { scope in
-            scope.setTag(value: Config.buildConfiguration, key: "build-configuration")
-            scope.setEnvironment(Config.buildConfiguration)
-        }
+        // if enableDebugLogging is true, log to sentry during local development
+        SentryManager.shared.start(enableDebugLogging: false)
 
         // example usage:
-        // SentrySDK.capture(message: "This app uses Sentry!")
+        // SentryManager.shared.captureMessage("This app uses Sentry!")
     }
     @State private var isAppSettingsViewShowing = false
     @State private var isModalStepTwoEnabled = false
