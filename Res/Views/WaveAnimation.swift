0c5d45d59316869a96d78f9eee9f969d22a77780
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2024-05-22 13:21:30 -0700

    Made the waveHeight configurable

--- Res/Views/WaveAnimation.swift
+++ Res/Views/WaveAnimation.swift
@@ -8,15 +8,18 @@
 import SwiftUI
 
 struct WaveAnimation: View {
+    
+    @Binding var height: Double
     @State private var waveOffset = Angle(degrees: 0)
     
     var body: some View {
-        Wave(offset: waveOffset)
+        Wave(offset: waveOffset, waveHeight: height / 100)
             .stroke(Color.green, lineWidth: 3.5)
             .onAppear { startAnimation() }
     }
     
     private func startAnimation() {
+//        withAnimation(.linear(duration: 1 - height / 100).repeatForever(autoreverses: false)) {
         withAnimation(.linear(duration: 1).repeatForever(autoreverses: false)) {
             waveOffset = Angle(degrees: -360)
         }
@@ -24,7 +27,9 @@ struct WaveAnimation: View {
 }
 
 struct Wave: Shape {
+    
     var offset: Angle
+    let waveHeight: Double
     
     // Animatable data for the shape
     var animatableData: Double {
@@ -37,7 +42,7 @@ struct Wave: Shape {
         var p = Path()
         
         // Define the wave height as a small fraction of the view's height
-        let waveHeight = 0.015 * rect.height
+        let actualWaveHeight = waveHeight * rect.height
         
         // Set a fixed vertical position for the thin wave line
         let yOffSet = rect.height / 2
@@ -47,20 +52,29 @@ struct Wave: Shape {
         let endAngle = offset + Angle(degrees: 360 + 10)
         
         // Move to the initial point of the wave on the left side of the view
-        p.move(to: CGPoint(x: 0, y: yOffSet + waveHeight * CGFloat(sin(offset.radians))))
+        p.move(to: CGPoint(x: 0, y: yOffSet + actualWaveHeight * CGFloat(sin(offset.radians))))
         
         // Iterate over the angles to draw the wave pattern
         for angle in stride(from: startAngle.degrees, through: endAngle.degrees, by: 5) {
+            
             // Calculate the x-coordinate based on the current angle
             let x = CGFloat((angle - startAngle.degrees) / 360) * rect.width
-            // Add a line to the current point of the wave, adjusting y-coordinate based on sine function
-            p.addLine(to: CGPoint(x: x, y: yOffSet + waveHeight * CGFloat(sin(Angle(degrees: angle).radians))))
+            
+            // Add a line to the current point of the wave,
+            // adjusting y-coordinate based on sine function
+            p.addLine(to: CGPoint(x: x, y: yOffSet + actualWaveHeight
+                                  * CGFloat(sin(Angle(degrees: angle).radians))))
         }
         
         return p // Return the constructed path representing the wave shape
     }
+    
+    init(offset: Angle, waveHeight: Double = 0.015) {
+        self.offset = offset
+        self.waveHeight = waveHeight
+    }
 }
 
 #Preview {
-    WaveAnimation()
+    WaveAnimation(height: .constant(2))
 }
